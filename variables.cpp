3f1101b4ea187d8196966eb350391e353a748258
Breye Foka <breyefokalagloire@gmail.com>
2024-10-24 13:20:17 +0100

    variables

---
+++ variables.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int number=5;
+int result;
+
+int main(){
+    result= number*5;
+
+    cout<<"the resulte if the calculation is:"<<endl;
+    cout<<result;
+    cout<<endl;
+}
