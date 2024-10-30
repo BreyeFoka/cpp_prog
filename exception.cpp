dbf213bc7c87c50988859e689eba5bce2e406d0d
Breye Foka <breyefokalagloire@gmail.com>
2024-10-30 13:38:39 +0100

    clear

--- exception.cpp
+++ exception.cpp
@@ -3,33 +3,33 @@ using namespace std;
 
 int main()
 {
-    // int age;
+    int age;
     string errorMessage = "Please enter a valid number ";
 
-    // try
-    // {
-    //     /* code */
-    //     cout << "Please enter your age: " << endl;
-    //     cin >> age;
-    //     cout << "Your age is: " << age << endl;
-    //     if (age < 0)
-    //     {
-    //         throw errorMessage;
-    //     }
-    //     if (age > 18)
-    //     {
-    //         /* code */
-    //         cout << "You can vote" << endl;
-    //     }
-    //     else
-    //     {
-    //         cout << "unfortunately you can't vote" << endl;
-    //     }
-    // }
-    // catch (string thrown_error)
-    // {
-    //     cout << thrown_error << endl;
-    // }
+    try
+    {
+        /* code */
+        cout << "Please enter your age: " << endl;
+        cin >> age;
+        cout << "Your age is: " << age << endl;
+        if (age < 0)
+        {
+            throw errorMessage;
+        }
+        if (age > 18)
+        {
+            /* code */
+            cout << "You can vote" << endl;
+        }
+        else
+        {
+            cout << "unfortunately you can't vote" << endl;
+        }
+    }
+    catch (string thrown_error)
+    {
+        cout << thrown_error << endl;
+    }
 
     // program for division
 
